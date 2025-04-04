a4e72b2f4dc5db067ff8c43288e7b4d83d15340c
Alan Ampo <alanampo@gmail.com>
2025-04-04 21:33:45 +0200

    aaaa

--- class_lib/scripts.php
+++ class_lib/scripts.php
@@ -1,28 +1,6 @@
 <!-- jQuery 3.1.1 -->
 <?php
 $version = 12;
-
-$filePath = $_SERVER['DOCUMENT_ROOT'] . '/.env';
-if (!file_exists($filePath)) {
-	throw new Exception("Archivo .env no encontrado.");
-}
-
-$lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-foreach ($lines as $line) {
-	if (strpos($line, '=') !== false) {
-		list($name, $value) = explode('=', $line, 2);
-		$name = trim($name);
-		$value = str_replace('"', '', trim($value));
-
-		if (!array_key_exists($name, $_ENV)) {
-			putenv("$name=$value");
-			$_ENV[$name] = $value;
-			$_SERVER[$name] = $value;
-		}
-	}
-}
-
-
 ?>
 <script src="./js/jquery.min.js" type="text/javascript"></script>
 <script src="./js/jquery-ui.min.js" type="text/javascript"></script>
--- class_lib/sesionSecurity.php
+++ class_lib/sesionSecurity.php
@@ -2,7 +2,25 @@
 session_name("roel-control");
 session_start();
 date_default_timezone_set("America/Santiago");
+$filePath = $_SERVER['DOCUMENT_ROOT'] . '/.env';
+if (!file_exists($filePath)) {
+  throw new Exception("Archivo .env no encontrado.");
+}
 
+$lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($lines as $line) {
+  if (strpos($line, '=') !== false) {
+    list($name, $value) = explode('=', $line, 2);
+    $name = trim($name);
+    $value = str_replace('"', '', trim($value));
+    
+    if (!array_key_exists($name, $_ENV)) {
+      putenv("$name=$value");
+      $_ENV[$name] = $value;
+      $_SERVER[$name] = $value;
+    }
+  }
+}
 $version = 12;
 header('Content-type: text/html; charset=utf-8');
 if (!isset($_SESSION["roel-token"]) || !isset($_COOKIE["roel-token"])) {
